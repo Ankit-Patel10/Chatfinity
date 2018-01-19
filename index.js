8d5178e6e7550459bcb60bfaad82348a16af1ede
Ankit Patel <ankitp5469@gmail.com>
2018-01-18 21:49:18 -0500

    'commit'

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -14,6 +16,6 @@ io.on('connection', function (socket) {
 });
 
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
